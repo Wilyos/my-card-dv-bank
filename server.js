4376e2b5bdc0ffce7ecd20187e083f01ef728f29
Wil Bohorquez <wilyos99@gmail.com>
2025-08-29 11:21:38 -0500

    fix routes

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+// Middleware para normalizar rutas con o sin slash final
+app.use((req, res, next) => {
+  if (req.path.endsWith('/') && req.path.length > 1) {
+    res.redirect(301, req.path.slice(0, -1));
+  } else {
+    next();
+  }
+});
+
 app.get('/:landing', (req, res) => {
   const landing = req.params.landing; // Ej: "my-personal-card1"
   const folder = landing;
